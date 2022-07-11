6383626210aaecc692d23221a3c1e370fa19b84d
Erik Bogdan <erik.bogdan@gmail.com>
2022-07-12 00:22:18 +0200

    Initial commit with VITE

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      asdas
+    </div>
+  )
+}
+
+export default App
