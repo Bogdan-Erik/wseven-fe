b84694c9082557d226a0377ab704ed2903b6615d
Erik Bogdan <erik.bogdan@gmail.com>
2022-10-13 17:33:29 +0200

    Modified gitignore and apislice

--- src/redux/apiSlice.tsx
+++ src/redux/apiSlice.tsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://wseven.bogdandev.hu/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKEND_URL }),
   tagTypes: ['Post'],
   endpoints: (builder) => ({}),
 })
