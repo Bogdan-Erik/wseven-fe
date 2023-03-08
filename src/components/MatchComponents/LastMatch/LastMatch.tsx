df66ea00bfe8edc4c909c48b5179a40c99fb6468
Erik Bogdan <erik.bogdan@gmail.com>
2023-03-08 12:02:24 +0100

    Javított utolsó meccs képe

--- src/components/MatchComponents/LastMatch/LastMatch.tsx
+++ src/components/MatchComponents/LastMatch/LastMatch.tsx
@@ -19,8 +19,8 @@ export const LastMatch = ({
     <div className={twMerge(`holder-space ${position === 'right' ? 'text-right' : 'text-left'}`)}>
       <span className="title">Előző meccs</span>
       <div className="flex mt-[5px]">
-        <div className={`${position === 'right' ? ' flex justify-end order-2' : 'mr-[10px] order-1'}`}><img src={TransformServerImage(data?.img)} className="max-w-[30px]" /></div>
-        <div className={`${position === 'right' ? 'self-center text-xs ml-auto mr-[10px] order-1' : 'self-center text-xs order-2'}`}>
+        <div className={`${position === 'right' ? ' flex justify-end order-2' : 'mr-[10px] order-1'}`}><img src={TransformServerImage(data?.img)} className="w-[40px] h-[40px] rounded-full" /></div>
+        <div className={`font-[500] ${position === 'right' ? 'self-center text-xs ml-auto mr-[10px] order-1' : 'self-center text-xs order-2'}`}>
           {data?.name && (<div>{data?.name}</div>)}
           <div>{data?.result}</div>
         </div>
--- src/utils/TransformServerImage.tsx
+++ src/utils/TransformServerImage.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 
 const TransformServerImage = (imagePath:any) => {
-  return (!imagePath.includes('http') ? import.meta.env.VITE_BACKEND_URL + 'storage/' + imagePath : imagePath);
+  return (!imagePath.includes('http') ? import.meta.env.VITE_DO_IMAGE_HOST + imagePath : imagePath);
 }
 
 export default TransformServerImage;
