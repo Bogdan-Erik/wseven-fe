c076486234d839d5043b45c3f172968fe477bca1
Erik Bogdan <erik.bogdan@gmail.com>
2023-03-09 17:23:03 +0100

    Javított bet max height

--- src/components/BetComponents/OddsItem/OddsItem.tsx
+++ src/components/BetComponents/OddsItem/OddsItem.tsx
@@ -10,7 +10,7 @@ export interface OddsItemProps {
 }
 
 export const OddsItem = ({ odds, editMark }: OddsItemProps): JSX.Element => {
-  const innerClass = twMerge(`bg-light-green rounded-md px-[13px] py-[5px] text-xs w-[82px] text-white font-[500] ${editMark ? 'flex' : ''}`);
+  const innerClass = twMerge(`bg-light-green rounded-md px-[13px] py-[5px] text-xs w-[82px] text-white font-[500] max-h-[26px] ${editMark ? 'flex' : ''}`);
   return (
     <div className={innerClass}>
       <span className="font-icomoon icon-stat text-green  mr-[10px]"> </span>{parseFloat(odds).toFixed(2)} {editMark && (<div className="ml-[5px]"><FontAwesomeIcon icon={faPencil} /></div>)}
